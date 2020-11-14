a7a00b7347d024eac82de4094f45938c5ab7975c
FaheemBuksh-FB <72087381+FaheemBuksh-FB@users.noreply.github.com>
2020-11-14 05:31:04 -0800

    First

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 $(document).ready(()=>{
-alert();
+    $("#btn").click(()=>{
+        $(".section").css("display","block");
+        $.ajax({
+            url:'https://fakestoreapi.com/products',
+            method: 'GET',
+            success: function(data){
+                console.log(data)
 
-$("#btn").click(function(){
-    
-$.ajax({
-    url: 'https://jsonplaceholder.typicode.com/posts',
-    method: 'GET',
-    success: function(data){
-        console.log(data);
-    }
-});
+            var content='';
+            var i = 0;
 
-});
-
-
+            $.each(data,function(key,val){
+                // if(i == 3){
+                // content += '<br>';
+                // i= 0;
+                // }
+                // ++i;
+                content += '<div class="cards"><img src="'+val.image+'"alt=""><p>'+val.category+'</p></div>'
+            });
+            $('.section').append(content);
+            }
+        })
+    });
 });
